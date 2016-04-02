3b6f1da8aefe06bea7b05cfd7d51280b910763bc
Mark Rizko <mark@rizkocircle.com>
2016-04-01 21:56:48 -0700

    Initial Commit

---
+++ BCGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BCGame
+//
+//  Created by Marcus Rizko on 4/1/16.
+//  Copyright © 2016 Marcus Rizko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
